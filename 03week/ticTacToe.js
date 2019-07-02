b241fd4c200408286d23fe8117b0c57c5aa0dc5a
pmzarate <patriciamarakzarate@yahoo.com>
2019-07-02 10:47:40 -0500

    ticTacToe work with tutor

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -1,23 +1,19 @@
-'use strict';
 
+'use strict';
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-//board spaces
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
 
-//player's move
 let playerTurn = 'X';
 
-// location of spaces on board
 function printBoard() {
   console.log('   0  1  2');
   console.log('0 ' + board[0].join(' | '));
@@ -27,90 +23,81 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-// ways to win horizontally
 function horizontalWin() {
-  // Your code here
-if ((board[0][0] === playerTurn && board[0][1]===playerTurn && board[0][2] === playerTurn)||
-  (board[1][0]=== playerTurn && board[1][1]===playerTurn && board[1][2]===playerTurn) ||
-  (board[2][0]===playerTurn && board[2][1] ===playerTurn&& board [2][2]===playerTurn)) {
-      return true;}
-        else {
-          return false;}}
-  
-// ways to win vertically
+  if ((board[0][0] && board[0][1] && board[0][2]) ||
+  (board[1][0] && board[1][1] && board[1][2]) ||
+  (board[2][0] && board[2][1] && board[2][2])) {
+    return true
+  }
+}
+
 function verticalWin() {
-  // Your code here
-if ((board[0][1] === playerTurn&& board[1][1]===playerTurn && board[2][1]===playerTurn) ||
-  (board[0][0]===playerTurn && board[1][0]===playerTurn && board[2][0]===playerTurn) ||
-  (board[0][2]===playerTurn && board[1][2]===playerTurn && board[2][2]== playerTurn)) {
-      return true;}
-      else {
-        return false;}}
-
-// ways to win diagonally
-function diagonalWin() {
-  // Your code here
-  if ((board[0][0] ===playerTurn && board[1][1]===playerTurn && board[2][2]===playerTurn) ||
-  (board[2][0]===playerTurn && board [1][1]===playerTurn && board [0][2]===playerTurn)) {
-    return true;}
-    else {
-      return false;}}
-
-// is space empty
-function isEmpty (row, column) {
-  if (board[row][column]== ""){
-    return true;}
-    else { return false;}}
-
-// check for win either vert, hori, dia
-function checkForWin() {
-  // Your code here
-  if (verticalWin() || horizontalWin() || diagonalWin()) {
-    return true;}  
-    else {
-      return false;}
-}
-
-// changes players turn
-function SwitchPlayer(playerTurn) {
-  if(playerTurn === 'x') {
-      return 'X';
-  } else (playerTurn === 'o'); {
-      return 'O';
-  }
-
-// if move is valid and space is empty => move piece to location.  then check to see if the move wins the game  
-function ticTacToe(row, column) {
-  // Your code here
-  if (isValid(row, column)) {
-    if (isEmpty(row,colum)) {
-      movePiece(row, column);
-      if (checkForWin()) {
-        console.log("player "+ playerTurn + " wins");
-      }
-      else {
+  if ((board[0][0] && board[1][0] && board[2][0]) ||
+    (board[0][1] && board[1][1] && board[2][1]) ||
+    (board[0][2] && board[1][2] && board[2][2])) {
+      return true
     }
-  }   else { console.log ("choose another square");
-}
 }
 
-// player's move
-function movePiece(x, y) {
-  board[x][y] = playerTurn;
+function diagonalWin() {
+  if ((board[0][0] && board[1][1] && board [2][2]) ||
+    (board[2][0] && board[1][1] && board[0][2])){
+      return true
+    }
 }
 
-//check to see if location is valid or available
-function isValid(row, column) {
-    if (
-    (row == 0 || row == 1 || row == 2) &&
-    (column == 0 || column == 1 || column == 2)) {
-    return true;
-    } else {
-    return false;
+function checkForWin() {
+  if (horizontalWin(),true || verticalWin(),true || diagonalWin(),true) {
+    return true
+  } else {
+    console.log("it's a tie")
   }
 }
 
-//prompt for turn
+//sitch turns
+function switchPlayer () {
+  if (playerTurn === 'X') {
+    playerTurn = 'O'
+  } else {
+    playerTurn = 'X'
+  }
+}
+
+
+function ticTacToe(row,column) {
+  // Your code here
+  if(validate(row,column)) {
+    if (emptySpace(row,column)) {
+      board[row][column] = playerTurn;
+      switchPlayer();
+    }  else { 
+      console.log("game over")
+    }
+  }
+}
+
+
+//validate input
+function validate (row, column) {
+  if ( (row==0 )|| (row ==1 )||( row == 2)) {
+    if ( (column==0 )|| (column ==1 )||( column == 2)) {
+      return true
+      } else { 
+        console.log("invalid move")
+    } 
+  }
+}
+//check empty space
+function emptySpace(row,column) {
+  if (board[row][column] === ' ') {
+    return true
+  }
+}
+
+
+
+
+
 function getPrompt() {
   printBoard();
   console.log("It's Player " + playerTurn + "'s turn.");
@@ -120,7 +107,10 @@ function getPrompt() {
       getPrompt();
     });
   });
+
 }
+
+
 
 // Tests
 
@@ -152,5 +142,7 @@ if (typeof describe === 'function') {
     });
   });
 } else {
-  getPrompt()
+
+  getPrompt();
+
 }
