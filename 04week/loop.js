0bd840f77fd6cb9f515164792c3c073700f913c8
pmzarate <patriciamarakzarate@yahoo.com>
2019-07-17 19:28:18 -0500

    start loops homework

---
+++ 04week/loop.js
@@ -0,0 +1,13 @@
+// 
+const person = {firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'};
+
+//const isOdd = pe/\d+{4}/
+
+let birthYr = person.birthDate [person.birthDate.length -1];
+ 
+console.log (birthYr)
+
+//for (person.birthDate.length === % 2!)
+
+const birthYr = 
+person.birthDate
