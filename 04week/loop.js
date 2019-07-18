5fed6117b81bcb9bee18db92b8421ba32d922d86
pmzarate <patriciamarakzarate@yahoo.com>
2019-07-18 09:41:13 -0500

    some work on loops

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,13 +1,27 @@
-// 
-const person = {firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'};
+// let numbers = 0;
+// do {
+//   numbers += 1;
+//   console.log(numbers);
+// } while (numbers < 1000);
 
-//const isOdd = pe/\d+{4}/
+
+
+
+const person = {firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'};
 
 let birthYr = person.birthDate [person.birthDate.length -1];
  
-console.log (birthYr)
 
 //for (person.birthDate.length === % 2!)
 
-const birthYr = 
-person.birthDate
+// const isOdd = person.birthDate.length(!0 === %2);
+// console.log (isOdd)
+
+
+const arrayOfPerson = [firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'],  
+[firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender: 'female'], 
+[firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'],
+[firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female'], 
+[firstName: 'Jane', lastName: 'Doe', birthDate: 'Jan 5, 1925', gender:'female']
+
+
